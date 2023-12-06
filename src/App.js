4467033439b1d5c619dd7ec2e35b093fde959c49
Liebniz <anders.leif.loren@liebniz.com>
2023-12-06 23:18:08 +0100

    toggle background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./styles/AppStyles.css";
+
+import Menu from "./Menu";
+
+const App = () => {
+  const [theme, setTheme] = useState("light-theme");
+  const [buttonColor, setButtonColor] = useState("dark");
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+      setButtonColor("bright");
+    } else {
+      setTheme("light-theme");
+      setButtonColor("dark");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      <button className={`btn-custom ${buttonColor} m-2 mx-3`} onClick={toggleTheme}>
+        {theme == "light-theme" ? "Dark" : "Light"}
+      </button>
+      <Menu />
     </div>
   );
-}
+};
 
 export default App;
